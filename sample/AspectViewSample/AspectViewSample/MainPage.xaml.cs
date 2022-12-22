8a92d467b7ef9d2ef0c7a828a15f1210f8925953
Julian Ewers <ewerspej@tcd.ie>
2022-12-22 11:18:27 +0100

    implemented AspectView control

---
+++ sample/AspectViewSample/AspectViewSample/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace AspectViewSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public string VideoSource => GetVideo();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private static string GetVideo()
+        {
+            if (DeviceInfo.Platform == DevicePlatform.Android)
+            {
+                return "https://github.com/dotnet/maui-samples/blob/main/6.0/UserInterface/Handlers/CreateHandlerDemo/VideoDemos/Resources/Raw/AndroidVideo.mp4?raw=true";
+            }
+
+            if (DeviceInfo.Platform == DevicePlatform.iOS)
+            {
+                return "https://github.com/dotnet/maui-samples/blob/main/6.0/UserInterface/Handlers/CreateHandlerDemo/VideoDemos/Resources/Raw/AppleVideo.mp4?raw=true";
+            }
+
+            return string.Empty;
+        }
+    }
+}
---
+++ src/epj.AspectView.Forms/AspectView.xaml.cs
@@ -0,0 +1,66 @@
+﻿using CommunityToolkit.Diagnostics;
+using System.Diagnostics;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace epj.AspectView.Forms
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AspectView
+    {
+        public double AspectRatio
+        {
+            get => (double)GetValue(AspectRatioProperty);
+            set
+            {
+                Guard.IsGreaterThan(value, 0.0);
+                SetValue(AspectRatioProperty, value);
+            }
+        }
+
+        public static readonly BindableProperty AspectRatioProperty = BindableProperty.Create(nameof(AspectRatio), typeof(double), typeof(AspectView), 1.0);
+
+        public AspectView()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+
+            if (width < 1.0 || height < 1.0)
+            {
+                return;
+            }
+
+            if (HorizontalOptions.Alignment == LayoutAlignment.Fill)
+            {
+                var newHeight = Width * AspectRatio;
+                if (!HeightRequest.Equals(newHeight))
+                {
+                    HeightRequest = newHeight;
+                    LogUpdate();
+                }
+                return;
+            }
+
+            if (VerticalOptions.Alignment == LayoutAlignment.Fill)
+            {
+                Guard.IsGreaterThan(AspectRatio, 0.0);
+                var newWidth = HeightRequest / AspectRatio;
+
+                if (!WidthRequest.Equals(newWidth))
+                {
+                    WidthRequest = newWidth;
+                    LogUpdate();
+                }
+            }
+        }
+
+        private void LogUpdate()
+        {
+            Debug.WriteLine($"{nameof(AspectView)} updated: {nameof(AspectRatio)}: {AspectRatio}, {nameof(WidthRequest)}: {WidthRequest}, {nameof(HeightRequest)}: {HeightRequest}");
+        }
+    }
+}
